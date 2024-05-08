89c0ee62ca07d2d68495643d154e3ebe7b105ef8
UsersAdmins <daarkovv@mail.ru>
2024-05-08 21:39:02 +0300

    Add files via upload

---
+++ zadaniya/zadanie1.py
@@ -0,0 +1,7 @@
+vid = input()
+years = input()
+name = input()
+
+print('Это', vid, f"по кличке '{name}'.", "Возраст:", years, "года" )
+
+#https://github.com/UsersAdmins/zadanie1-2.git
---
+++ zadaniya/zadanie2.py
@@ -0,0 +1,4 @@
+print('Задание 1. Выпишите все этапы развития человека')
+etapi = input()
+
+print('Australopithecus', 'Homo habilis', 'Homo erectus', 'Homo neanderthalensis', 'Homo sapiens', sep='=>d')
